23b211e13aeedd07df4f7e582853bf6e430edde2
dxx <1427168220@qq.com>
2021-12-24 17:02:15 +0800

    1

--- worker.js
+++ worker.js
@@ -1,3 +1,20 @@
-this.addEventListener('message', (event) => {
-  console.log(1)
+// this.addEventListener('message', (event) => {
+//   console.log(1)
+// })
+
+new Promise((resolve, reject) => {
+  // return 1
+  resolve(1)
+  // return 1
+  // resolve(1)
+  // throw 'error'
+  // reject('111')
+  console.log('contain')
+}).then(ret => {
+  console.log('ok2：' + ret)
+  return 2222
+}).then(ret => {
+  console.log(ret)
+}).catch(e => {
+  console.log(e)
 })
